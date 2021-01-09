272dae0a2cf856eb1d47b24ba2f42ec5b830ada6
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-01-09 08:52:58 +0100

    Implemented GraphEdges as unique pointers (Task 4)

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -28,9 +28,9 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(edge);
+    _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
@@ -54,7 +54,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return _childEdges[index].get();
 
     ////
     //// EOF STUDENT CODE
